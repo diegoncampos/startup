5ea44f200b69921f876e4c86fcbbbdf57aa3e371
diegoncampos <diego.nahuel.campos@gmail.com>
2015-09-25 22:23:41 -0300

    V 2.0 Lista y datos

--- 05-angularjs/js/moviesBD.js
+++ 05-angularjs/js/moviesBD.js
@@ -1,9 +1,9 @@
 var movies = [
 {
-  tittle: 'The Simpsons',
-  director: 'David Silverman',
-  duration: '125',
-  photo: 'https://upload.wikimedia.org/wikipedia/en/a/a0/Simpsons_final_poster.png'
+  tittle: "The Simpsons Movie",
+  director: "David Silverman",
+  duration: "125",
+  photo: "https://upload.wikimedia.org/wikipedia/en/a/a0/Simpsons_final_poster.png"
 },
 {
   tittle: "Jurassic World",
@@ -16,5 +16,11 @@ var movies = [
  director: "Richard Oswald",
  duration: "106",
  photo: "http://www.labutaca.net/imagenes/wp-content/original/2012_06/ted-cartel-1.jpg"
+},
+{
+  tittle: "The Avengers",
+  director: "Joss Whedon",
+  duration: "142",
+  photo: "http://globoxrentals.com/site/wp-content/uploads/2014/11/the_avengers_poster_by_creativeorange-d4yrg5d.jpg"
 }
 ];
--- 05-angularjs/js/script.js
+++ 05-angularjs/js/script.js
@@ -2,4 +2,25 @@ var app = angular.module('appMovie', []);
 
 app.controller('movieCtrl', function($scope) {
 	$scope.movies = movies;
+
+	$scope.selectedMovie={
+		tittle:"",
+		nombre:""
+	};
+
+	$scope.get = function(movie){
+		$scope.details = "Movie Details:";
+		$scope.tittle = "Titulo: " + movie.tittle;
+		$scope.director = "Director: " + movie.director;
+		$scope.duration = "Duration: " + movie.duration;
+		$scope.photo = movie.photo;		
+
+	};
+
+	// $scope.addMovie = function(tittle, director, duration, photo){
+	// 	var newMovie = 
+
+	// };
+
+
 });
