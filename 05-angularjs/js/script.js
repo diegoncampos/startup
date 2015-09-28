06e9ebb90a195f77fe38ff4f295d532607da1e2c
diegoncampos <diego.nahuel.campos@gmail.com>
2015-09-27 22:03:29 -0300

    Add new movie and delete functions.

--- 05-angularjs/js/script.js
+++ 05-angularjs/js/script.js
@@ -1,4 +1,5 @@
 var app = angular.module('appMovie', []);
+var selectedMovie = "";
 
 app.controller('movieCtrl', function($scope) {
 	$scope.movies = movies;
@@ -9,18 +10,65 @@ app.controller('movieCtrl', function($scope) {
 	};
 
 	$scope.get = function(movie){
-		$scope.details = "Movie Details:";
-		$scope.tittle = "Titulo: " + movie.tittle;
-		$scope.director = "Director: " + movie.director;
-		$scope.duration = "Duration: " + movie.duration;
-		$scope.photo = movie.photo;		
+		$scope.getDetails = "Movie Details:";
+		$scope.getTittle = "Titulo: " + movie.tittle;
+		$scope.getDirector = "Director: " + movie.director;
+		$scope.getDuration = "Duration: " + movie.duration;
+		$scope.getPhoto = movie.photo;	
+
+		$scope.edTittle = movie.tittle;
+		$scope.edDirector = movie.director;
+		$scope.edDuration = movie.duration;
+		$scope.edPhoto = movie.photo;	
+
+		selectedMovie = movie;
 
 	};
 
-	// $scope.addMovie = function(tittle, director, duration, photo){
-	// 	var newMovie = 
+	$scope.addMovie = function(){
+		
+		$scope.newMovie = [{tittle : $scope.newTittle, 
+			director : $scope.newDirector, 
+			duration : $scope.newDuration, 
+			photo : $scope.newPhoto}];
 
-	// };
+			$scope.movies = $scope.movies.concat($scope.newMovie);
+
+			$scope.newTittle = "";
+			$scope.newDirector = "";
+			$scope.newDuration = "";
+			$scope.newPhoto = "";
+
+		};
+
+		$scope.deleteMovie = function(){  //working
+
+			var index = $scope.movies.indexOf(selectedMovie);
+
+			var r = confirm("Delete movie?");
+			if (r === true) {
+				
+				$scope.movies.splice(index,1); 
+
+				$scope.edTittle = "";
+				$scope.edDirector = "";
+				$scope.edDuration = "";
+				$scope.edPhoto = "";
+			} 
 
 
-});
+		};
+
+		$scope.editMovie = function(movie){   //working
+			$scope.edTittle = movie.tittle;
+			$scope.edDirector = movie.director;
+			$scope.edDuration = movie.duration;
+			$scope.edPhoto = movie.photo;		
+
+		};
+
+		
+
+
+
+	});
